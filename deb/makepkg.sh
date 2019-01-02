949cc50de3c3d1947daa220628a78fd5bf44b5cf
Anton Sochnev <an.sochnev@yandex.ru>
2019-01-02 16:49:03 +0300

    make version configurable (only for deb package)

--- deb/makepkg.sh
+++ deb/makepkg.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 
-DIR=helloworld-1.0.1
+DIR=helloworld-1.0.$TRAVIS_BUILD_NUMBER
 
 mkdir -p $DIR/DEBIAN;
 mkdir -p $DIR/usr/bin
 
 cp deb/* $DIR/DEBIAN
 cp helloworld $DIR/usr/bin
+echo "Version: 1.0.$TRAVIS_BUILD_NUMBER" >> $DIR/DEBIAN/control
 
 dpkg -b $DIR
 
